4cbf4dce5caecadb285653a533edc0be328fe931
ire4564 <ire4564@gmail.com>
2022-11-29 15:13:16 +0900

    modify: meata tag position

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,19 @@ import Meta from "../components/Meta";
 const MyDocument = () => {
   return (
     <Html lang="en">
-      <Meta />
       <Head>
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/github-dark.min.css"
+        />
+        <meta name="og:description" content={"나만의 프로그래머 아카이브"} />
+        <meta name="keywords" content="ireArchive Record Blog" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          property="og:image"
+          content={
+            "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FPoLeS%2FbtrSlYCgWOq%2FooUrHNpGBH10ldSD8OpIG1%2Fimg.png"
+          }
         />
       </Head>
       <body>

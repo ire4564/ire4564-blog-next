06a485bf651c36753fdad4b9aad1006f95c5cfce
ire4564 <ire4564@gmail.com>
2022-11-29 15:19:49 +0900

    modify: open tag test

--- pages/_document.js
+++ pages/_document.js
@@ -5,24 +5,10 @@ const MyDocument = () => {
   return (
     <Html lang="en">
       <Head>
+        <Meta />
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/github-dark.min.css"
-        />
-        <meta property="og:title" content={"IRE Archive"} />
-        <meta
-          property="og:description"
-          content={"나만의 프로그래머 아카이브"}
-        />
-        <meta
-          property="og:url"
-          content={"https://ire4564-blog-next.vercel.app/"}
-        />
-        <meta
-          property="og:image"
-          content={
-            "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FPoLeS%2FbtrSlYCgWOq%2FooUrHNpGBH10ldSD8OpIG1%2Fimg.png"
-          }
         />
       </Head>
       <body>
